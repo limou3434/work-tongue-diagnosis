7cecb64efbac7963b19006a34486c36d37241058
limou3434 <898738804@qq.com>
2025-05-03 12:20:28 +0800

    初始化项目

---
+++ src/test/java/cn/com/edtechhub/worktonguediagnosis/WorkTongueDiagnosisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.edtechhub.worktonguediagnosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTongueDiagnosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
